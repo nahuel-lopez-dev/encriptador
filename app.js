6b760463cb96d15c5cacd3725c25624f25d0ff03
Nahuel-DevOne <nahuel.profesor@gmail.com>
2022-01-23 22:08:59 -0300

    comenzando con js

---
+++ app.js
@@ -0,0 +1,15 @@
+/** Forma de organización **/
+/***** 1. Entidades (No se va a usar en esta app) *****/ 
+/***** 2. Variables y selectores *****/
+/***** 3. Funciones *****/
+/***** 4. Eventos *****/
+/***** 5. Lógica (No va a necesitar una sección aparte) *****/ 
+
+/** variables y selectores */
+
+
+/** funciones */
+
+
+
+/** eventos */
